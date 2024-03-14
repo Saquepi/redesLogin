5a998fc7ee95ecc28f94bfd85ae0e1bef04d3006
Saquepi <145624844+Saquepi@users.noreply.github.com>
2024-03-13 20:43:48 -0600

    RedesInicLogin

---
+++ redesInicSesion/src/main/java/com/mycompany/redesinicsesion/RedesInicSesion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.redesinicsesion;
+
+/**
+ *
+ * @author LABORATORIO 04
+ */
+public class RedesInicSesion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
